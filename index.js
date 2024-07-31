5d0e2759692400219793406cf116706e901c770b
lucassantos22 <lucas.afsantos@hotmail.com>
2024-07-30 23:12:38 -0300

    chore: remove unused const

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import chalk from 'chalk';
 import inquirer from 'inquirer';
 import chalkAnimation from 'chalk-animation';
 import { exec } from 'child_process';
-import path from 'path';
 
 const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 
